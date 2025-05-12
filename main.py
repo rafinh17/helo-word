146e3899c436aed70c4b66236f7875c7622bf2ad
rafinh17 <fafa.macisilva15@gmail.com>
2025-05-12 17:18:43 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+#operadores boolenos verdadeiro(true) ou flaso (false)
+
+a = 3 > 4
+#print(a)
+#sinal de diferente (!=)
+b = 7 != 7
+#print(b)
+
+
+#comparação composta 
+composto =9 == 10 - 1
+#print(composto)
+
+composto2 = 5 + 4 <= 11 - 2
+#print(composto2)
+
+#ordem aritmetica 
+#exemplo de divisão com saida float 
+divisão_saida_float = 4 / 2 
+#print(f'esse é o resultado da divisão com / saida float {divisão_saida_float}')
+
+divisão_saida_int = 4 // 2
+#print('Esse é o resultado da divisão saida inteiro',divisão_saida_int)
+
+divisão_resto = 4 % 2
+#print(divisão_resto) 
+
+resultado = 3 + 4 * 2
+print(f'Esse é o resultado do calculo de 3 + 4 * 2 é : {resultado}')
